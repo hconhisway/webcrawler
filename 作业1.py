7b6eda76541e58269172bd2e9b7cdf72425d21a6
hconhisway <hatch.on27.com>
2020-07-06 21:25:09 +0800

    2

--- 作业1.py
+++ 作业1.py
@@ -1,7 +1,7 @@
 '''
 @Author: 韩畅
 @Date: 2020-07-06 20:47:56
-@LastEditTime: 2020-07-06 20:58:20
+@LastEditTime: 2020-07-06 21:17:04
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \webcrawler-1\作业1.py
@@ -14,3 +14,4 @@ ur2 = "http://google.com"
 response2 = urllib.request.urlopen(ur2)
 print(response2.read())
 print("hello")
+print("hello2")
