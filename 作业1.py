28764d6d294b22a80d82d9c4c378a691309b8306
hconhisway <hatch.on27.com>
2020-07-06 23:46:07 +0800

    2

--- 作业1.py
+++ 作业1.py
@@ -1,7 +1,7 @@
 '''
 @Author: 韩畅
 @Date: 2020-07-06 20:47:56
-@LastEditTime: 2020-07-06 21:17:04
+@LastEditTime: 2020-07-06 23:45:12
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \webcrawler-1\作业1.py
@@ -10,8 +10,6 @@ import urllib.request
 url = "http://www.baidu.com"
 response = urllib.request.urlopen(url)
 print(response.read())
-ur2 = "http://google.com"
+ur2 = "file:///C:/Users/hconh/Desktop/local.html"
 response2 = urllib.request.urlopen(ur2)
 print(response2.read())
-print("hello")
-print("hello2")
